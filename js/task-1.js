6267b5bc6c086c93f557e80e737bad8d29fa593f
Anton <goydenko.30@gmail.com>
2024-06-28 12:48:15 +0300

    add .js HW-4

---
+++ js/task-1.js
@@ -0,0 +1,13 @@
+function isEnoughCapacity(products, containerSize) {
+    let totalProductsInContainer = 0;
+    for (const key in products) {
+        totalProductsInContainer += products[key];
+    }
+
+    return totalProductsInContainer <= containerSize;
+}
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14));
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7));
